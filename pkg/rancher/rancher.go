b56f4d3a166f30943162145b034af6fd290f4ec5
Tom Lebreux <tom.lebreux@suse.com>
2025-08-25 16:38:01 -0400

    Use channels

--- pkg/rancher/rancher.go
+++ pkg/rancher/rancher.go
@@ -151,10 +151,6 @@ func New(ctx context.Context, clientConfg clientcmd.ClientConfig, opts *Options)
 		return nil, err
 	}
 
-	// back-fill the context with the CAPI factory when the relevant
-	// CRDs are available
-	go wranglerContext.ManageDeferredCAPIContext(ctx)
-
 	// Check for deprecated RKE1 resources in the cluster
 	if err := validateRKE1Resources(wranglerContext); err != nil {
 		return nil, fmt.Errorf("rke1 pre-upgrade validation failed: %w", err)
